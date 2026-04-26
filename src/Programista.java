35a89aef91715461d11e1be6aaec3a8515349c4d
LidiaKowalewska <s35334@pjwstk.edu.pl>
2026-04-26 16:40:21 +0200

    Klasa Programista

--- src/Programista.java
+++ src/Programista.java
@@ -1,3 +1,41 @@
-public class Programista {
+public class Programista extends Pracownik {
+
+    private String glownyJezyk;
+    private int liczbaRepozytoriow;
+
+
+    public Programista(String idPracownika, String imie, String nazwisko, double stawkaBazowa, String glownyJezyk, int liczbaRepozytoriow) {
+        super(idPracownika, imie, nazwisko, stawkaBazowa);
+        this.glownyJezyk = glownyJezyk;
+        this.liczbaRepozytoriow = liczbaRepozytoriow;
+    }
+
+    public String getGlownyJezyk(){
+        return glownyJezyk;
+    }
+
+    public int getLiczbaRepozytoriow(){
+        return liczbaRepozytoriow;
+    }
+
+    @Override
+    public double obliczKosztMiesieczny(){
+        return super.obliczKosztMiesieczny() + (super.getStawkaBazowa() * 0.40);
+    }
+
+
+    @Override
+    public String przedstawSie(){
+        String opis = "";
+        opis = super.przedstawSie();
+        opis =  (" moj glowny jezyk programowania to " + glownyJezyk + " w ktorym mam " + liczbaRepozytoriow + " repozytoriow");
+        return opis;
+    }
+
+    public String wypiszTechnologie(){
+        String opis = "";
+        opis = "Głowna technologia " + glownyJezyk;
+        return opis;
+    }
 
 }

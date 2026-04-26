fdf4815fd272a63207694745452951061d5aec7f
LidiaKowalewska <s35334@pjwstk.edu.pl>
2026-04-26 16:56:45 +0200

    klasa tester

--- src/Tester.java
+++ src/Tester.java
@@ -1,2 +1,46 @@
-public class Tester {
+public class Tester extends Pracownik {
+    private boolean czyAutomatyzujacy;
+    private int liczbaScenariuszy;
+
+    public Tester(String idPracownika, String imie, String nazwisko, double stawkaBazowa,boolean czyAutomatyzujacy, int liczbaScenariuszy) {
+        super(idPracownika, imie, nazwisko, stawkaBazowa);
+        this.czyAutomatyzujacy = czyAutomatyzujacy;
+        this.liczbaScenariuszy = liczbaScenariuszy;
+    }
+
+    public boolean getCzyAutomatyzujacy() {
+        return czyAutomatyzujacy;
+    }
+    public int getLiczbaScenariuszy() {
+        return liczbaScenariuszy;
+    }
+
+    @Override
+    public double obliczKosztMiesieczny(){
+        return super.obliczKosztMiesieczny() + (super.getStawkaBazowa() * 0.10);
+    }
+
+    @Override
+    public String przedstawSie(){
+        String opis = "";
+        opis = super.przedstawSie();
+        opis =  (" Jestem testerem " + czyAutomatyzujacy + " Zajmuje sie " + liczbaScenariuszy + " Scenariuszami");
+        return opis;
+    }
+
+    public void uruchomRaportTestow() {
+        String typTestera = czyAutomatyzujacy ? "Automatyzujący" : "Manualny";
+
+        System.out.println("--- RAPORT TESTERA ---");
+        System.out.println("Typ: " + typTestera);
+        System.out.println("Liczba przygotowanych scenariuszy: " + liczbaScenariuszy);
+
+        if (czyAutomatyzujacy && liczbaScenariuszy > 10) {
+            System.out.println("Status: Wysoka wydajność automatyzacji.");
+        } else {
+            System.out.println("Status: Standardowy proces testowy.");
+        }
+        System.out.println("-----------------------");
+    }
+
 }
